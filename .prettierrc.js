38816e01ac7efe6c6780af6818e423c0ec471bd2
Gustaf Gunér <gustaf@guner.se>
2018-11-13 12:10:17 +0100

    Basic Spotify Web API call now working

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  semi: false,
+  semi: true,
   tabWidth: 2,
   singleQuote: true,
   trailingComma: 'all',
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import * as React from 'react'
-import './App.css'
+import * as queryString from 'query-string';
+import * as React from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
 
-const App = () => (
-  <>
-    <div>
+import './App.css';
+
+const spotifyApi = new SpotifyWebApi();
+
+const getNowPlaying = () => {
+  spotifyApi
+    .getMyCurrentPlaybackState()
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error: any) => {
+      console.log(error);
+    });
+};
+
+const App = () => {
+  const parsed = queryString.parse(window.location.search);
+  const accessToken = parsed.access_token as string | undefined;
+  if (accessToken) {
+    spotifyApi.setAccessToken(accessToken);
+  }
+  getNowPlaying();
+  return (
+    <>
       <h1>Welcome</h1>
-    </div>
-  </>
-)
+    </>
+  );
+};
 
-export default App
+export default App;
