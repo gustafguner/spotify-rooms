f5020adc1021c500d3ff2b05f4d697a8ab0c116f
Gustaf Gunér <gustaf@guner.se>
2018-11-15 10:52:56 +0100

    Started styling the Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,37 @@
 import * as React from 'react';
+import styled from 'react-emotion';
+
+const HeaderWrapper = styled('header')`
+  width: 100%;
+  height: 70px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: white;
+  display: flex;
+  box-shadow: 0 3px 7px rgba(0, 0, 0, 0.08);
+  padding: 0 25px;
+  align-items: center;
+`;
+
+const HeaderWrapperPadding = styled('div')`
+  padding-bottom: 70px;
+`;
+
+const Logo = styled('div')`
+  width: 100px;
+  height: 30px;
+  background-color: red;
+`;
 
 const Header: React.SFC<{}> = () => (
-  <div>
-    <p>Header</p>
-    <a href="http://localhost:8888/login">Login with Spotify</a>
-  </div>
+  <>
+    <HeaderWrapperPadding />
+    <HeaderWrapper>
+      <Logo />
+      <a href="http://localhost:8888/login">Login with Spotify</a>
+    </HeaderWrapper>
+  </>
 );
 
 export default Header;
