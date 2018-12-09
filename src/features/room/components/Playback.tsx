97b7ef85dfe42d07f77c806ec237d7b808c56069
Gustaf Gunér <gustaf@guner.se>
2018-12-09 19:08:27 +0100

    Blurred background for current playing song in room

--- src/features/room/components/Playback.tsx
+++ src/features/room/components/Playback.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styled from 'react-emotion';
 import { colors } from 'src/styles';
+import Blur from 'react-blur';
 
 interface PlaybackProps {
   track: Track;
@@ -22,19 +23,77 @@ interface Artist {
   name: string;
 }
 
+const Wrapper = styled('div')({
+  width: '100%',
+  height: 230,
+  position: 'relative',
+});
+
 const Container = styled('div')({
   display: 'flex',
   width: '100%',
-  height: 220,
-  backgroundColor: colors.BLACK,
+  alignItems: 'center',
+  padding: 25,
+  height: '100%',
+  position: 'relative',
 });
 
-const Image = styled('img')({});
+const CoverImageWrapper = styled('div')({
+  width: 180,
+  flexBasis: 180,
+  flexShrink: 0,
+  height: 180,
+  boxShadow: '0 0 30px rgba(0, 0, 0, 0.5)',
+});
+
+const CoverImage = styled('img')({
+  float: 'left',
+  width: '100%',
+  height: '100%',
+});
+
+const BackgroundBlur = styled(Blur)({
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+  left: 0,
+  top: 0,
+});
+
+const DarkFilter = styled('div')({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  backgroundColor: 'rgba(0, 0, 0, 0.36)',
+  backgroundImage:
+    'linear-gradient(to right, rgba(0,0,0,0.9), rgba(0,0,0,0.10))',
+});
+
+const TrackInfo = styled('div')({
+  width: '100%',
+  marginLeft: 40,
+});
+
+const TrackName = styled('h1')({
+  margin: 0,
+});
 
 const Playback: React.SFC<PlaybackProps> = ({ track }) => (
-  <Container>
-    <Image src={track.images[0].url} />
-  </Container>
+  <Wrapper>
+    <BackgroundBlur img={track.images[0].url} blurRadius={60} />
+    <DarkFilter />
+
+    <Container>
+      <CoverImageWrapper>
+        <CoverImage src={track.images[0].url} />
+      </CoverImageWrapper>
+      <TrackInfo>
+        <TrackName>{track.name}</TrackName>
+      </TrackInfo>
+    </Container>
+  </Wrapper>
 );
 
 export { Playback };
---
+++ src/types/react-blur.d.ts
@@ -0,0 +1,15 @@
+declare module 'react-blur' {
+  import * as React from 'react';
+  interface BlurProps {
+    blurRadius?: number;
+    children?: JSX.Element;
+    className?: string;
+    enableStyles?: boolean;
+    img: string;
+    onLoadFunction?: () => void;
+    shouldResize?: boolean;
+    resizeInterval?: number;
+  }
+  const Blur: React.ComponentType<BlurProps>;
+  export default Blur;
+}
