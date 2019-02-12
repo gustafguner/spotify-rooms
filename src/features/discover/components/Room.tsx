2da8a2d7973ae24dbb9dd5b69e09b248b18dcbaf
Gustaf Gunér <gustaf@guner.se>
2019-02-12 10:43:01 +0100

    Roll animation of current track WIP

--- src/features/discover/components/Room.tsx
+++ src/features/discover/components/Room.tsx
@@ -18,12 +18,14 @@ const Container = styled('div')({
   marginLeft: 15,
   marginRight: 15,
   position: 'relative',
-  borderRadius: 5,
+  borderRadius: 4,
   overflow: 'hidden',
 });
 
 const Link = styled(ReactLink)({
   textDecoration: 'none',
+  position: 'relative',
+  zIndex: 1,
 });
 
 const Spin = keyframes({
@@ -32,15 +34,16 @@ const Spin = keyframes({
 });
 
 const CoverImageWrapper = styled('div')({
-  width: 150,
-  flexBasis: 150,
+  width: 120,
+  flexBasis: 120,
   flexShrink: 0,
-  height: 150,
-  boxShadow: '0 0 30px rgba(0, 0, 0, 0.5)',
+  height: 120,
+  boxShadow: '0 0 15px rgba(0, 0, 0, 0.5)',
   borderRadius: '50%',
   overflow: 'hidden',
   animation: `${Spin} 1.8s linear infinite`,
   position: 'relative',
+  zIndex: 1,
 });
 
 const CoverImage = styled('img')({
@@ -84,22 +87,52 @@ const DarkFilter = styled('div')({
   width: '100%',
   height: '100%',
   backgroundColor: 'rgba(0, 0, 0, 0.36)',
-  backgroundImage: 'linear-gradient(to top, rgba(0,0,0,0.9), rgba(0,0,0,0.10))',
+  backgroundImage:
+    'linear-gradient(to top, rgba(0,0,0,0.45), rgba(0,0,0,0.10))',
 });
 
 const CoverContainer = styled('div')({
   width: '100%',
   display: 'flex',
   justifyContent: 'center',
-  paddingTop: 15,
+  alignItems: 'center',
+  position: 'relative',
+  height: 160,
 });
 
 const MetaInfoContainer = styled('div')({
+  width: '100%',
+  height: 100,
   padding: 15,
-  marginTop: 10,
   position: 'relative',
   display: 'flex',
   justifyContent: 'center',
+  flexFlow: 'row wrap',
+  backgroundColor: colors.ALMOST_BLACK,
+  '&:before': {
+    position: 'absolute',
+    content: '""',
+    left: 0,
+    bottom: 0,
+    height: '100%',
+    width: 15,
+    backgroundImage: `linear-gradient(to left, rgba(0,0,0,0), ${
+      colors.ALMOST_BLACK
+    } 40%)`,
+    zIndex: 1,
+  },
+  '&:after': {
+    position: 'absolute',
+    content: '""',
+    right: 0,
+    bottom: 0,
+    height: '100%',
+    width: 15,
+    backgroundImage: `linear-gradient(to right, rgba(0,0,0,0), ${
+      colors.ALMOST_BLACK
+    } 40%)`,
+    zIndex: 1,
+  },
 });
 
 const Name = styled('div')({
@@ -107,42 +140,39 @@ const Name = styled('div')({
   color: colors.WHITE,
   textAlign: 'center',
   fontSize: 18,
+  fontWeight: 600,
+  flexBasis: '100%',
+  flexShrink: 0,
+  flexGrow: 0,
+  marginBottom: 8,
+  height: 26,
+});
+
+const Roll = keyframes({
+  '0%': { left: 0 },
+  '100%': { left: '100%', transform: 'translateX(-100%)' },
+});
+
+const TrackContainer = styled('div')({
+  flexBasis: '100%',
+  flexShrink: 0,
+  flexGrow: 0,
+  position: 'relative',
+});
+
+const Track = styled('div')({
+  color: 'rgba(255,255,255,0.2)',
+  whiteSpace: 'nowrap',
+  position: 'absolute',
+  fontSize: 15,
+  fontWeight: 300,
+  animation: `${Roll} 3s infinite alternate linear`,
 });
 
 const Room: React.SFC<Props> = ({ room }) => {
   const isTrack = room.playback && room.playback.id !== null;
   return (
     <Container>
-      <ReactCSSTransitionReplace
-        transitionName="cross-fade"
-        transitionEnterTimeout={1000}
-        transitionLeaveTimeout={1000}
-        overflowHidden={false}
-        transitionAppear={true}
-        style={{
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-        }}
-        className={'absolute-react-replace'}
-      >
-        {isTrack && room.playback.images[0].url !== null ? (
-          <BackgroundWrapper key={`track-bg-${room.playback.id}`}>
-            <BackgroundBlur
-              img={room.playback.images[0].url}
-              blurRadius={60}
-              enableStyles={true}
-            />
-          </BackgroundWrapper>
-        ) : (
-          <BackgroundWrapper key="track-default-bg">
-            <DefaultBackground />
-          </BackgroundWrapper>
-        )}
-      </ReactCSSTransitionReplace>
-
-      <DarkFilter />
-
       <Link to={`/room/${room.id}`}>
         <CoverContainer>
           <CoverImageWrapper>
@@ -152,9 +182,45 @@ const Room: React.SFC<Props> = ({ room }) => {
               <DefaultCoverImage />
             )}
           </CoverImageWrapper>
+          <ReactCSSTransitionReplace
+            transitionName="cross-fade"
+            transitionEnterTimeout={1000}
+            transitionLeaveTimeout={1000}
+            overflowHidden={false}
+            transitionAppear={true}
+            style={{
+              width: '100%',
+              height: '100%',
+              position: 'absolute',
+            }}
+            className={'absolute-react-replace'}
+          >
+            {isTrack && room.playback.images[0].url !== null ? (
+              <BackgroundWrapper key={`track-bg-${room.playback.id}`}>
+                <BackgroundBlur
+                  img={room.playback.images[0].url}
+                  blurRadius={60}
+                  enableStyles={true}
+                />
+              </BackgroundWrapper>
+            ) : (
+              <BackgroundWrapper key="track-default-bg">
+                <DefaultBackground />
+              </BackgroundWrapper>
+            )}
+          </ReactCSSTransitionReplace>
+
+          <DarkFilter />
         </CoverContainer>
         <MetaInfoContainer>
           <Name>{room.name}</Name>
+          {isTrack ? (
+            <TrackContainer>
+              <Track>{room.playback.name}</Track>
+            </TrackContainer>
+          ) : (
+            <TrackContainer>No playback</TrackContainer>
+          )}
         </MetaInfoContainer>
       </Link>
     </Container>
